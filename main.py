6905fa627ad279448f33ba0fbeab59c0fc050752
Pavlov <pavlovalexey2003@gmail.com>
2025-05-26 23:53:57 +0300

    property

--- main.py
+++ main.py
@@ -543,117 +543,165 @@
 # a1.shet_def()
 
 # Занятие 19
-# class Account:
-#     rate_usd = 0.013  # Курс рубля по отношению к доллару
-#     rate_eur = 0.011
-#     suffix = "RUB"
-#     suffix_usd = "USD"
-#     suffix_eur = "EUR"
-#
-#     def __init__(self, surname, num, percent, value):
-#         self.surname = surname
-#         self.num = num
-#         self.percent = percent
-#         self.value = value
-#         print(f"Счёт #{self.num} принадлежащий {self.surname} был открыт")
-#         print("*" * 50)
-#
-#     def __del__(self):
-#         print("*" * 50)
-#         print(f"Счёт #{self.num} принадлежащий {self.surname} был закрыт")
-#
-#     @property
-#     def surname(self):
-#         return self.surname
-#
-#     @surname.setter
-#     def surname(self, surname):
-#         self.surname = surname
-#
-#     @staticmethod
-#     def convert(value, rate):
-#         return value * rate
-#
-#     @classmethod
-#     def set_usd_rate(cls, rate):  # rate - новый курс
-#         cls.rate_usd = rate  # Заменяем курс!
-#
-#     @classmethod
-#     def set_eur_rate(cls, rate):  # rate - новый курс
-#         cls.rate_eur = rate  # Заменяем курс!
-#
-#     def convert_to_usd(self):
-#         usd_val = Account.convert(self.value, Account.rate_usd)
-#         print(f"Состояние счёта: {usd_val} {Account.suffix_usd}")
-#
-#     def convert_to_eur(self):
-#         eur_val = Account.convert(self.value, Account.rate_eur)
-#         print(f"Состояние счёта: {eur_val} {Account.suffix_eur}")
-#
-#     def edit_owner(self, surname):
-#         self.surname = surname
-#
-#     def add_percent(self):
-#         self.value += self.value * self.percent
-#         print("Проценты были успешно начислены!")
-#         self.print_balance()
-#
-#     def withdraw_money(self, val):
-#         if val > self.value:
-#             print(f"К сожалению у вас нет {val} {Account.suffix}")
-#         else:
-#             self.value -= val
-#             print(f"{val} {Account.suffix} было успешно снято!")
-#
-#         self.print_balance()
-#
-#     def add_money(self, val):
-#         self.value += val
-#         print(f"{val} {Account.suffix} было успешно добавлено!")
-#         self.print_balance()
-#
-#     def print_balance(self):
-#         print(f"Текущий баланс {self.value} {Account.suffix}")
-#
-#     def print_info(self):
-#         print("Информация о счёте:")
-#         print("-" * 20)
-#         print(f"#{self.num}")
-#         print(f"Владелец: {self.surname}")
-#         self.print_balance()
-#         print(f"Проценты: {self.percent:.0%}")
-#         print("-" * 20)
-#
-#
-# acc = Account("Долгих", "12345", 0.03, 1000)
-# acc.print_info()
-# acc.convert_to_usd()
-# acc.convert_to_eur()
-# print()
-# Account.set_usd_rate(2)  # Меняем статическое свойство класса Account
-# Account.set_eur_rate(3)
-#
-# acc.convert_to_usd()
-# acc.convert_to_eur()
-# print()
-# acc.edit_owner("Дона")
-# acc.print_info()
-# print()
-#
-# acc.add_percent()
-# print()
-#
-# acc.withdraw_money(900)
-# print()
-#
-# acc.withdraw_money(200)
-# print()
-#
-# acc.add_money(5000)
-# print()
-#
-# acc.withdraw_money(3000)
-# print()
+class Account:
+    rate_usd = 0.013  # Курс рубля по отношению к доллару
+    rate_eur = 0.011
+    suffix = "RUB"
+    suffix_usd = "USD"
+    suffix_eur = "EUR"
+
+    def __init__(self, surname, num, percent, value):
+        self.surname = surname
+        self.num = num
+        self.percent = percent
+        self.value = value
+        print(f"Счёт #{self.num} принадлежащий {self.surname} был открыт")
+        print("*" * 50)
+
+    def __del__(self):
+        print("*" * 50)
+        print(f"Счёт #{self.num} принадлежащий {self.surname} был закрыт")
+
+    @property  # Первый способ!
+    def surname(self):
+        return self.surname
+
+    @surname.setter
+    def surname(self, surname):
+        self.surname = surname
+
+    @property
+    def num(self):
+        return self.num
+
+    @num.setter
+    def num(self, num):
+        self.surname = num
+
+    @property
+    def percent(self):
+        return self.percent
+
+    @percent.setter
+    def percent(self, percent):
+        self.percent = percent
+
+    @property
+    def value(self):
+        return self.value
+
+    @value.setter
+    def value(self, value):
+        self.value = value
+
+    def get_surname(self):  # Второй способ!
+        return self.surname
+
+    def set_surname(self, surname):
+        self.surname = surname
+
+    def get_num(self):  # Второй способ!
+        return self.surname
+
+    def set_num(self, num):
+        self.num = num
+
+    def get_percent(self):  # Второй способ!
+        return self.surname
+
+    def set_percent(self, percent):
+        self.percent = percent
+
+    def get_value(self):  # Второй способ!
+        return self.surname
+
+    def set_value(self, value):
+        self.value = value
+
+    @staticmethod
+    def convert(value, rate):
+        return value * rate
+
+    @classmethod
+    def set_usd_rate(cls, rate):  # rate - новый курс
+        cls.rate_usd = rate  # Заменяем курс!
+
+    @classmethod
+    def set_eur_rate(cls, rate):  # rate - новый курс
+        cls.rate_eur = rate  # Заменяем курс!
+
+    def convert_to_usd(self):
+        usd_val = Account.convert(self.value, Account.rate_usd)
+        print(f"Состояние счёта: {usd_val} {Account.suffix_usd}")
+
+    def convert_to_eur(self):
+        eur_val = Account.convert(self.value, Account.rate_eur)
+        print(f"Состояние счёта: {eur_val} {Account.suffix_eur}")
+
+    def edit_owner(self, surname):
+        self.surname = surname
+
+    def add_percent(self):
+        self.value += self.value * self.percent
+        print("Проценты были успешно начислены!")
+        self.print_balance()
+
+    def withdraw_money(self, val):
+        if val > self.value:
+            print(f"К сожалению у вас нет {val} {Account.suffix}")
+        else:
+            self.value -= val
+            print(f"{val} {Account.suffix} было успешно снято!")
+
+        self.print_balance()
+
+    def add_money(self, val):
+        self.value += val
+        print(f"{val} {Account.suffix} было успешно добавлено!")
+        self.print_balance()
+
+    def print_balance(self):
+        print(f"Текущий баланс {self.value} {Account.suffix}")
+
+    def print_info(self):
+        print("Информация о счёте:")
+        print("-" * 20)
+        print(f"#{self.num}")
+        print(f"Владелец: {self.surname}")
+        self.print_balance()
+        print(f"Проценты: {self.percent:.0%}")
+        print("-" * 20)
+
+
+acc = Account("Долгих", "12345", 0.03, 1000)
+acc.print_info()
+acc.convert_to_usd()
+acc.convert_to_eur()
+print()
+Account.set_usd_rate(2)  # Меняем статическое свойство класса Account
+Account.set_eur_rate(3)
+
+acc.convert_to_usd()
+acc.convert_to_eur()
+print()
+acc.edit_owner("Дона")
+acc.print_info()
+print()
+
+acc.add_percent()
+print()
+
+acc.withdraw_money(900)
+print()
+
+acc.withdraw_money(200)
+print()
+
+acc.add_money(5000)
+print()
+
+acc.withdraw_money(3000)
+print()
 
 # print("\nЗадание №20")
 # # # Задание №20
@@ -696,96 +744,96 @@
 
 # print("\nЗадание №21")
 # # # Задание №21
-
-class Student:
-    def __init__(self, name1, name2):
-        self.name1 = name1
-        self.name2 = name2
-        self.m = self.Model()
-        self.p = self.Processor()
-        self.me = self.Memory()
-
-    # @property
-    # def name1(self):
-    #     return self.name1
-    #
-    # @name1.setter
-    # def name1(self, name1):
-    #     if not isinstance(name1, str):
-    #         raise TypeError("Не строка!")
-    #     else:
-    #         self.name1 = name1
-    #
-    # @property
-    # def name2(self):
-    #     return self.name2
-    #
-    # @name2.setter
-    # def name2(self, name2):
-    #     if not isinstance(name2, str):
-    #         raise TypeError("Не строка!")
-    #     else:
-    #         self.name2 = name2
-
-    def __str__(self):
-        print(f"{self.name1} => {self.m}, {self.p}, {self.me}\n"
-              f"{self.name2} => {self.m}, {self.p}, {self.me}")
-
-    def print_info(self):
-        print(f"{self.name1} => {self.m}, {self.p}, {self.me}\n"
-              f"{self.name2} => {self.m}, {self.p}, {self.me}")
-
-    class Model:
-        model = 'HP'
-
-        def __str__(self):
-            return self.model
-
-    class Processor:
-        processor = 'i7'
-
-        def __str__(self):
-            return self.processor
-
-    class Memory:
-        memory = "16"
-
-        def __str__(self):
-            return self.memory
-
-        # @property
-        # def model(self):
-        #     return self.model
-        #
-        # @model.setter
-        # def model(self, model):
-        #     if not isinstance(model, str):
-        #         raise TypeError("Не строка!")
-        #     else:
-        #         self.model = model
-        #
-        # @property
-        # def processor(self):
-        #     return self.model
-        #
-        # @processor.setter
-        # def processor(self, processor):
-        #     if not isinstance(processor, str):
-        #         raise TypeError("Не строка!")
-        #     else:
-        #         self.processor = processor
-        #
-        # @property
-        # def memory(self):
-        #     return self.model
-        #
-        # @memory.setter
-        # def memory(self, memory):
-        #     if not isinstance(memory, str):
-        #         raise TypeError("Не строка!")
-        #     else:
-        #         self.memory = memory  # Через p #
-
-
-one = Student('Лёша', 'Настя')
-one.print_info()
+#
+# class Student:
+#     def __init__(self, name1, name2):
+#         self.name1 = name1
+#         self.name2 = name2
+#         self.m = self.Model()
+#         self.p = self.Processor()
+#         self.me = self.Memory()
+#
+#     # @property
+#     # def name1(self):
+#     #     return self.name1
+#     #
+#     # @name1.setter
+#     # def name1(self, name1):
+#     #     if not isinstance(name1, str):
+#     #         raise TypeError("Не строка!")
+#     #     else:
+#     #         self.name1 = name1
+#     #
+#     # @property
+#     # def name2(self):
+#     #     return self.name2
+#     #
+#     # @name2.setter
+#     # def name2(self, name2):
+#     #     if not isinstance(name2, str):
+#     #         raise TypeError("Не строка!")
+#     #     else:
+#     #         self.name2 = name2
+#
+#     def __str__(self):
+#         print(f"{self.name1} => {self.m}, {self.p}, {self.me}\n"
+#               f"{self.name2} => {self.m}, {self.p}, {self.me}")
+#
+#     def print_info(self):
+#         print(f"{self.name1} => {self.m}, {self.p}, {self.me}\n"
+#               f"{self.name2} => {self.m}, {self.p}, {self.me}")
+#
+#     class Model:
+#         model = 'HP'
+#
+#         def __str__(self):
+#             return self.model
+#
+#     class Processor:
+#         processor = 'i7'
+#
+#         def __str__(self):
+#             return self.processor
+#
+#     class Memory:
+#         memory = "16"
+#
+#         def __str__(self):
+#             return self.memory
+#
+#         # @property
+#         # def model(self):
+#         #     return self.model
+#         #
+#         # @model.setter
+#         # def model(self, model):
+#         #     if not isinstance(model, str):
+#         #         raise TypeError("Не строка!")
+#         #     else:
+#         #         self.model = model
+#         #
+#         # @property
+#         # def processor(self):
+#         #     return self.model
+#         #
+#         # @processor.setter
+#         # def processor(self, processor):
+#         #     if not isinstance(processor, str):
+#         #         raise TypeError("Не строка!")
+#         #     else:
+#         #         self.processor = processor
+#         #
+#         # @property
+#         # def memory(self):
+#         #     return self.model
+#         #
+#         # @memory.setter
+#         # def memory(self, memory):
+#         #     if not isinstance(memory, str):
+#         #         raise TypeError("Не строка!")
+#         #     else:
+#         #         self.memory = memory  # Через p #
+#
+#
+# one = Student('Лёша', 'Настя')
+# one.print_info()
