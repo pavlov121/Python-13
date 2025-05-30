9ce038106ee53c7e06e01342469d15d8aa9733d2
Pavlov <pavlovalexey2003@gmail.com>
2025-05-30 19:07:32 +0300

    operand

--- main.py
+++ main.py
@@ -543,165 +543,165 @@
 # a1.shet_def()
 
 # Занятие 19
-class Account:
-    rate_usd = 0.013  # Курс рубля по отношению к доллару
-    rate_eur = 0.011
-    suffix = "RUB"
-    suffix_usd = "USD"
-    suffix_eur = "EUR"
-
-    def __init__(self, surname, num, percent, value):
-        self.surname = surname
-        self.num = num
-        self.percent = percent
-        self.value = value
-        print(f"Счёт #{self.num} принадлежащий {self.surname} был открыт")
-        print("*" * 50)
-
-    def __del__(self):
-        print("*" * 50)
-        print(f"Счёт #{self.num} принадлежащий {self.surname} был закрыт")
-
-    @property  # Первый способ!
-    def surname(self):
-        return self.surname
-
-    @surname.setter
-    def surname(self, surname):
-        self.surname = surname
-
-    @property
-    def num(self):
-        return self.num
-
-    @num.setter
-    def num(self, num):
-        self.surname = num
-
-    @property
-    def percent(self):
-        return self.percent
-
-    @percent.setter
-    def percent(self, percent):
-        self.percent = percent
-
-    @property
-    def value(self):
-        return self.value
-
-    @value.setter
-    def value(self, value):
-        self.value = value
-
-    def get_surname(self):  # Второй способ!
-        return self.surname
-
-    def set_surname(self, surname):
-        self.surname = surname
-
-    def get_num(self):  # Второй способ!
-        return self.surname
-
-    def set_num(self, num):
-        self.num = num
-
-    def get_percent(self):  # Второй способ!
-        return self.surname
-
-    def set_percent(self, percent):
-        self.percent = percent
-
-    def get_value(self):  # Второй способ!
-        return self.surname
-
-    def set_value(self, value):
-        self.value = value
-
-    @staticmethod
-    def convert(value, rate):
-        return value * rate
-
-    @classmethod
-    def set_usd_rate(cls, rate):  # rate - новый курс
-        cls.rate_usd = rate  # Заменяем курс!
-
-    @classmethod
-    def set_eur_rate(cls, rate):  # rate - новый курс
-        cls.rate_eur = rate  # Заменяем курс!
-
-    def convert_to_usd(self):
-        usd_val = Account.convert(self.value, Account.rate_usd)
-        print(f"Состояние счёта: {usd_val} {Account.suffix_usd}")
-
-    def convert_to_eur(self):
-        eur_val = Account.convert(self.value, Account.rate_eur)
-        print(f"Состояние счёта: {eur_val} {Account.suffix_eur}")
-
-    def edit_owner(self, surname):
-        self.surname = surname
-
-    def add_percent(self):
-        self.value += self.value * self.percent
-        print("Проценты были успешно начислены!")
-        self.print_balance()
-
-    def withdraw_money(self, val):
-        if val > self.value:
-            print(f"К сожалению у вас нет {val} {Account.suffix}")
-        else:
-            self.value -= val
-            print(f"{val} {Account.suffix} было успешно снято!")
-
-        self.print_balance()
-
-    def add_money(self, val):
-        self.value += val
-        print(f"{val} {Account.suffix} было успешно добавлено!")
-        self.print_balance()
-
-    def print_balance(self):
-        print(f"Текущий баланс {self.value} {Account.suffix}")
-
-    def print_info(self):
-        print("Информация о счёте:")
-        print("-" * 20)
-        print(f"#{self.num}")
-        print(f"Владелец: {self.surname}")
-        self.print_balance()
-        print(f"Проценты: {self.percent:.0%}")
-        print("-" * 20)
-
-
-acc = Account("Долгих", "12345", 0.03, 1000)
-acc.print_info()
-acc.convert_to_usd()
-acc.convert_to_eur()
-print()
-Account.set_usd_rate(2)  # Меняем статическое свойство класса Account
-Account.set_eur_rate(3)
-
-acc.convert_to_usd()
-acc.convert_to_eur()
-print()
-acc.edit_owner("Дона")
-acc.print_info()
-print()
-
-acc.add_percent()
-print()
-
-acc.withdraw_money(900)
-print()
-
-acc.withdraw_money(200)
-print()
-
-acc.add_money(5000)
-print()
-
-acc.withdraw_money(3000)
-print()
+# class Account:
+#     rate_usd = 0.013  # Курс рубля по отношению к доллару
+#     rate_eur = 0.011
+#     suffix = "RUB"
+#     suffix_usd = "USD"
+#     suffix_eur = "EUR"
+#
+#     def __init__(self, surname, num, percent, value):
+#         self.surname = surname
+#         self.num = num
+#         self.percent = percent
+#         self.value = value
+#         print(f"Счёт #{self.num} принадлежащий {self.surname} был открыт")
+#         print("*" * 50)
+#
+#     def __del__(self):
+#         print("*" * 50)
+#         print(f"Счёт #{self.num} принадлежащий {self.surname} был закрыт")
+#
+#     @property  # Первый способ!
+#     def surname(self):
+#         return self.surname
+#
+#     @surname.setter
+#     def surname(self, surname):
+#         self.surname = surname
+#
+#     @property
+#     def num(self):
+#         return self.num
+#
+#     @num.setter
+#     def num(self, num):
+#         self.surname = num
+#
+#     @property
+#     def percent(self):
+#         return self.percent
+#
+#     @percent.setter
+#     def percent(self, percent):
+#         self.percent = percent
+#
+#     @property
+#     def value(self):
+#         return self.value
+#
+#     @value.setter
+#     def value(self, value):
+#         self.value = value
+#
+#     def get_surname(self):  # Второй способ!
+#         return self.surname
+#
+#     def set_surname(self, surname):
+#         self.surname = surname
+#
+#     def get_num(self):  # Второй способ!
+#         return self.surname
+#
+#     def set_num(self, num):
+#         self.num = num
+#
+#     def get_percent(self):  # Второй способ!
+#         return self.surname
+#
+#     def set_percent(self, percent):
+#         self.percent = percent
+#
+#     def get_value(self):  # Второй способ!
+#         return self.surname
+#
+#     def set_value(self, value):
+#         self.value = value
+#
+#     @staticmethod
+#     def convert(value, rate):
+#         return value * rate
+#
+#     @classmethod
+#     def set_usd_rate(cls, rate):  # rate - новый курс
+#         cls.rate_usd = rate  # Заменяем курс!
+#
+#     @classmethod
+#     def set_eur_rate(cls, rate):  # rate - новый курс
+#         cls.rate_eur = rate  # Заменяем курс!
+#
+#     def convert_to_usd(self):
+#         usd_val = Account.convert(self.value, Account.rate_usd)
+#         print(f"Состояние счёта: {usd_val} {Account.suffix_usd}")
+#
+#     def convert_to_eur(self):
+#         eur_val = Account.convert(self.value, Account.rate_eur)
+#         print(f"Состояние счёта: {eur_val} {Account.suffix_eur}")
+#
+#     def edit_owner(self, surname):
+#         self.surname = surname
+#
+#     def add_percent(self):
+#         self.value += self.value * self.percent
+#         print("Проценты были успешно начислены!")
+#         self.print_balance()
+#
+#     def withdraw_money(self, val):
+#         if val > self.value:
+#             print(f"К сожалению у вас нет {val} {Account.suffix}")
+#         else:
+#             self.value -= val
+#             print(f"{val} {Account.suffix} было успешно снято!")
+#
+#         self.print_balance()
+#
+#     def add_money(self, val):
+#         self.value += val
+#         print(f"{val} {Account.suffix} было успешно добавлено!")
+#         self.print_balance()
+#
+#     def print_balance(self):
+#         print(f"Текущий баланс {self.value} {Account.suffix}")
+#
+#     def print_info(self):
+#         print("Информация о счёте:")
+#         print("-" * 20)
+#         print(f"#{self.num}")
+#         print(f"Владелец: {self.surname}")
+#         self.print_balance()
+#         print(f"Проценты: {self.percent:.0%}")
+#         print("-" * 20)
+#
+#
+# acc = Account("Долгих", "12345", 0.03, 1000)
+# acc.print_info()
+# acc.convert_to_usd()
+# acc.convert_to_eur()
+# print()
+# Account.set_usd_rate(2)  # Меняем статическое свойство класса Account
+# Account.set_eur_rate(3)
+#
+# acc.convert_to_usd()
+# acc.convert_to_eur()
+# print()
+# acc.edit_owner("Дона")
+# acc.print_info()
+# print()
+#
+# acc.add_percent()
+# print()
+#
+# acc.withdraw_money(900)
+# print()
+#
+# acc.withdraw_money(200)
+# print()
+#
+# acc.add_money(5000)
+# print()
+#
+# acc.withdraw_money(3000)
+# print()
 
 # print("\nЗадание №20")
 # # # Задание №20
@@ -837,3 +837,93 @@ print()
 #
 # one = Student('Лёша', 'Настя')
 # one.print_info()
+
+# Задание 22
+
+class Clock:
+    __DAY = 86400
+
+    def __init__(self, sec: int):
+        if not isinstance(sec, int):
+            raise ValueError("Секунды должны быть целым числом")
+        self.sec = sec % self.__DAY
+
+    def get_format_time(self):
+        s = self.sec % 60
+        m = (self.sec // 60) % 60
+        h = (self.sec // 3600) % 24
+        return f"{Clock.get_form(h)}:{Clock.get_form(m)}:{Clock.get_form(s)}"
+
+    @staticmethod
+    def get_form(x):
+        return str(x) if x > 9 else "0" + str(x)
+
+    def __add__(self, other):  # +
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом данных Clock")
+        return Clock(self.sec + other.sec)  # Clock(300)
+
+    def __eq__(self, other):
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом данных Clock")
+        return self.sec == other.sec
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __sub__(self, other):  # -
+        if not isinstance(other, Clock):
+            raise ValueError("Не целое число!")
+        return Clock(self.sec - other.sec)
+
+    def __mul__(self, other):  # -
+        if not isinstance(other, Clock):
+            raise ValueError("Не целое число!")
+        return Clock(self.sec * other.sec)
+
+    def __floordiv__(self, other):  # //
+        if not isinstance(other, Clock):
+            raise ValueError("Не целое число!")
+        return Clock(self.sec // other.sec)
+
+    def __mod__(self, other):  # %
+        if not isinstance(other, Clock):
+            raise ValueError("Не целое число!")
+        return Clock(self.sec % other.sec)
+
+    def __lt__(self, other):  # <
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом данных Clock")
+        return self.sec > other.sec
+
+    def __le__(self, other):  # <=
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом данных Clock")
+        return self.sec <= other.sec
+
+    def __gt__(self, other):  # >
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом данных Clock")
+        return self.sec > other.sec
+
+    def __ge__(self, other):  # <
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом данных Clock")
+        return self.sec >= other.sec
+
+
+c1 = Clock(100)
+c2 = Clock(200)
+# c4 = Clock(300)
+# c3 = c1 + c2 + c4
+print(c1.get_format_time())
+print(c2.get_format_time())
+# c1 += c2
+# print(c1.get_format_time())
+# print(c2.get_format_time())
+# print(c4.get_format_time())
+# print(c3.get_format_time())
+if c1 != c2:
+    print("Время разное")
+else:
+    print("Время одинаковое")
