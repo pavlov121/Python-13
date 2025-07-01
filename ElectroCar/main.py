e6b3e618d1cec7f44a510a51a15fa7e1bcea7e1b
Pavlov <pavlovalexey2003@gmail.com>
2025-07-01 20:28:57 +0300

    SQL

--- ElectroCar/main.py
+++ ElectroCar/main.py
@@ -1037,6 +1037,37 @@ from fileinput import filename
 # if __name__ == "__main__":
 #     main()
 
-# Занятие 27
+# Занятие 30
+import sqlite3
 
+with sqlite3.connect("education.db") as con:
+    cur = con.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS student(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    surname TEXT NOT NULL,
+    name TEXT NOT NULL,
+    patronymic TEXT NOT NULL,
+    age INTEGER NOT NULL CHECK(age >= 17 AND age < 50),
+    [group] TEXT NOT NULL,
+    FOREIGN KEY ([group]) REFERENCES groups(id) ON DELETE RESTRICT
+    )""")
 
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS groups(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    group_name TEXT NOT NULL
+    )""")
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS lessons(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        e TEXT NOT NULL
+        )""")
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS association(
+        group_id INTEGER,
+        lesson_id INTEGER,
+        PRIMARY KEY (group_id, lesson_id)
+        FOREIGN KEY (group_id) REFERENCES groups(id)
+        FOREIGN KEY (lesson_id) REFERENCES lessons(id)
+       ) """)
